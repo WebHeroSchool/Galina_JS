4ae607bcac678b1268bcc8e35df5a98d61dfe1aa
Galina Zhigalova <7447041@gmail.com>
2021-03-03 18:45:29 +0500

    внесла изменения в имя параметра

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let loader = document.getElementById('loader')
 let body = document.body;
 let params = new URLSearchParams(window.location.toString());
-let name = params.get('name') || 'GalinaZhigalova';
+let name = params.get('username') || 'GalinaZhigalova';
 let user = `https://api.github.com/users/${name}`;
 let date = new Date();
 console.log(user)
